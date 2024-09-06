0d1b7f43226ed5680cdcf7a7e8119013765e8e24
phananhvu1609 <phananhvu1609@gmail.com>
2024-09-06 16:30:14 +0200

    test baryon slider

--- Simulator/media/js/simulator.js
+++ Simulator/media/js/simulator.js
@@ -1511,11 +1511,11 @@
 			else if(c=='l') sim.omega_l.slider.find('.ui-slider-handle').focus();
 			else if(c=='i') window.location.href = switchHash();
 			else if(c=='f') sim.ps.toggleFullScreen();
-			else if(c=='1') {
+			else if(c=='o') {
 				sim.omega_b.setValue(sim.omega_b.value-0.025);
 				sim.ps.loadData('omega_b',sim.omega_b.value,sim.omega_c.value,sim.omega_l.value);
 			}
-			else if(c=='2') {
+			else if(c=='p') {
 				sim.omega_b.setValue(sim.omega_b.value+0.025);
 				sim.ps.loadData('omega_b',sim.omega_b.value,sim.omega_c.value,sim.omega_l.value);
 			}
